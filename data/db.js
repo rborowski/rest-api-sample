c1297c8b6dfe32a63ec527254fedda76a1ed22ee
Rafał Borowski <moderator9781@gmail.com>
2024-05-24 12:37:45 +0200

    Add atlas-ready code

--- data/db.js
+++ data/db.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
-let credentials = ""
+let mongoDB;
 
 async function dbInit() {
-  if (process.env.DB_PASSWORD) {
-    credentials = `${process.env.DB_USER}:${process.env.DB_PASSWORD}@`;
+  if (process.env.DB_ATLAS_URI) {
+    mongoDB = `${process.env.DB_ATLAS_URI}`;
+  } else {
+    mongoDB = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
   }
 
-  const mongoDB = `mongodb://${credentials}${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
   await mongoose.connect(mongoDB);
-  
+
   const db = mongoose.connection;
   db.on("error", console.error.bind(console, "MongoDB connection error: "));
-
 }
 //connect to database
-module.exports = dbInit()
+module.exports = dbInit();
